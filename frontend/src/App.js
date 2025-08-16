c7b9e7241467577841caf9cc74fc8156b4ea9e82
Hirusha Sasanka <100258430+IT21352676@users.noreply.github.com>
2025-08-16 21:58:27 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,12 @@ import HomePage from "./screens/homescreen";
 import AppointmentScreen from "./screens/appointment";
 import GrowthChartScreen from "./screens/growthscreen";
 import Blogs from "./screens/blog";
-import Profile from "./screens/profile"
-import ChildProfile from "./screens/childprofile"
+import Profile from "./screens/profile";
+import ChildProfile from "./screens/childprofile";
 import AddChildProfile from "./screens/addchildprofile";
-import SignIn from "./screens/signin"
+import SignIn from "./screens/signin";
 import SignUp from "./screens/signup";
-import SelectDepartment from "./screens/selectedepartment";
-
+import SelectDepartment from "./screens/selectdepartment";
 
 export default function App() {
   return (
@@ -21,14 +20,14 @@ export default function App() {
         <Route path="/splashscreen" element={<SplashScreen />} />
         <Route path="/loadingscreen" element={<LoadingScreen />} />
         <Route path="/homescreen" element={<HomePage />} />
-        <Route path="/appointments" element={<AppointmentScreen />} />   
-        <Route path="/growthscreen" element={<GrowthChartScreen />} />  
-        <Route path="/blogs" element={<Blogs />} />  
-        <Route path="/profile" element={<Profile />} />  
+        <Route path="/appointments" element={<AppointmentScreen />} />
+        <Route path="/growthscreen" element={<GrowthChartScreen />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/childprofile" element={<ChildProfile />} />
         <Route path="/addchildprofile" element={<AddChildProfile />} />
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
